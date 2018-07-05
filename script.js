0de97bbd113f7adc74730b8288a4c385f7c64135
Sophia <schen9981@gmail.com>
2018-07-04 21:35:10 -0600

    add in home and about page

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#name").fadeIn(2000);
+    $('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		navigation: true,
+		verticalCentered: false,
+		menu: true
+	});
+});
+
